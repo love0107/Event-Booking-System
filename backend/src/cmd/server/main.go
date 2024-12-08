bc1fdc913aed13202bea0b12bc69d86728570912
krazylovlesh <lovlesh@krazybee.com>
2024-12-08 20:00:52 +0530

    set up env file

--- backend/src/cmd/server/main.go
+++ backend/src/cmd/server/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"event-booking-backend/src/pkg/db"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 	// This is where the program starts
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// Initialize the database
+	db.InitDB()
+
+
+	// Create a Gin router
+	router := gin.Default()
+
+	// Define the routes
+
+
 }
--- backend/src/pkg/db/mysql.go
+++ backend/src/pkg/db/mysql.go
@@ -1,1 +1,5 @@
 package db
+
+func InitDB() {
+	// dsn := os.Getenv("DB_DSN") // get the DSN from the environment variable
+}
