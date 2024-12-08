f1e6370393875ee06f0c8c8f0e21820dacb63ddd
Lovlesh Baghel <63864359+love0107@users.noreply.github.com>
2024-12-08 17:53:08 +0530

    Delete migrations directory

--- migrations/001_create_events.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Event (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       name VARCHAR(255) NOT NULL,
-                       description TEXT,
-                       date VARCHAR(255),
-                       location VARCHAR(255),
-                       createdOn DATETIME DEFAULT CURRENT_TIMESTAMP,
-                       updatedOn DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
